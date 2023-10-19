4665412ad86139999dba86e59537ae155d43e396
Erick Nascimento <eriknascimento73@gmail.com>
2023-10-19 14:36:47 -0300

    Update script.js
    dados atualizados

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function countdown() {
     clearInterval(i);
     document.querySelector(
       ".countdown"
-    ).innerHTML = `<h1>Countdown had expired!</h1>`;
+    ).innerHTML = `<h1>O JOGO JÁ ESTÁ LIBERADO </h1>`;
   }
   //If end date is not before today date
   else {
